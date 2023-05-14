b95e389855b430b162983101069b210bebe86120
selvam <118274987+DevoloperSelvam@users.noreply.github.com>
2023-05-14 13:01:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+var h1 = document.createElement("h1");
+h1.innerHTML="covid 19 Tracker";
+h1.style.textAlign="center";
+h1.style.marginTop="60px";
+h1.style.paddingBottom="20px"
+document.body.append(h1);
+
+var div=document.createElement("div");
+div.style.textAlign="center";
+var input =document.createElement("input");
+input.setAttribute("type","text");
+input.setAttribute("id","country");
+input.style.width="30%";
+input.setAttribute("placeholder","Enter the countryname");
+
+var linebreak=document.createElement("br");
+var button = document.createElement("button");
+button.setAttribute("type","button");
+button.classList.add("btn","btn-primary");
+button.style.margin="10px";
+button.innerHTML="Search";
+button.addEventListener("click",covid);
+var rediv=document.createElement("div");
+rediv.setAttribute("id","timer");
+
+div.append(input,linebreak,button,rediv);
+document.body.append(div);
+
+async function covid() {
+  let res=document.getElementById("country").value;
+  console.log(res);
+  let url=`https://api.covid19api.com/dayone/country/${res}`;
+  let res1=await fetch(url);
+  let res2=await res1.json();
+  console.log(res2);
+  var index = res2.length-1;
+  var active = res2[index].Active ;
+  var confirmed = res2[index].Confirmed;
+  var death =res2[index].Deaths;
+  console.log(death);
+  console.log(confirmed);
+  console.log(active);
+  rediv.innerHTML=`
+  <h3>Total:${confirmed}</h3>
+  <h3>Active:${active}</h3>
+  <h3>Deaths:${death} </h3>`
+  
+
+};
